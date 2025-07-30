3d8119801953c7028b6402d53931786b8a117113
oscar mallen <oscar.mallen@my.utsa.edu>
2025-07-30 17:19:22 -0500

    removed unused files# removed unused files from cu stomer dashboard

--- src/app/customer/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CustomerDashboardComponent } from '@/components/blocks/customer-dashboard';
-
-export default function CustomerDashboardPage() {
-  return <CustomerDashboardComponent />;
-}
